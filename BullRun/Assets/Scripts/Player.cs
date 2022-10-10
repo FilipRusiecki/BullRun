6d42075832553c1bdb3122058a89186b8ea94264
Sean Whelan <seanwhelan117@gmail.com>
2022-10-10 11:02:29 +0100

    Added player movement and scrolling floor

--- BullRun/Assets/Scripts/Player.cs
+++ BullRun/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    int speed = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
     }
 }
