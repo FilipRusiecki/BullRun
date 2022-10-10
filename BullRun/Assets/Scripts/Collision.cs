efc5915f1eb9dd38895d5f28a9cdd0418f9c74dd
FilipRusiecki <57904846+FilipRusiecki@users.noreply.github.com>
2022-10-10 12:05:54 +0100

    added collisions with bull
    Co-Authored-By: DonalHowe <58332538+DonalHowe@users.noreply.github.com>

---
+++ BullRun/Assets/Scripts/Collision.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+
+  
+    public GameObject m_Bull;
+    public GameObject m_particle;
+    public Transform m_pos;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("bull"))
+        {
+            GameObject particle = Instantiate(m_particle, m_pos);
+            Debug.Log("collidede with bull");
+            Destroy(particle,2.0f);
+        }
+    }
+}
