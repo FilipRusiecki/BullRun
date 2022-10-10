538ad7e3b949cbcb5396abc7815dcbabdf1b6e92
PatrickDonnelly <c00236160@itcarlow.ie>
2022-10-10 13:40:29 +0100

    Score implemented

---
+++ BullRun/Assets/Scripts/Score.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    private int score;
+    private int temp;
+    private TMP_Text scoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        temp++;
+        if (temp >= 60)
+        {
+            temp = 0;
+
+            score += 10;
+        }
+    }
+}
