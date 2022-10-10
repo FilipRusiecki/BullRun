c06626fee37d0f7053ade0a6754c6aed1378919c
FilipRusiecki <57904846+FilipRusiecki@users.noreply.github.com>
2022-10-10 10:28:41 +0100

    jumping done

--- BullRun/Assets/Scripts/Player.cs
+++ BullRun/Assets/Scripts/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    [Header("Player Jumping")]
+    public float jumpForce = 0;
+    public int jumpCount = 0;
+    public int allowedJumps = 0;
+    public float gravityScale = 0;
+    public float fallingGravityScale = 0;
+    public bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded == true)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+          //  animator.SetBool("isJumping", true);
+            jumpCount += 1;
+            if (jumpCount == allowedJumps)
+            {
+                isGrounded = false;
+            }
+        }
+
     }
+
+
 }
